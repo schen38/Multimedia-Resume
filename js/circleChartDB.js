249455d4d78864c72f0e60d63d5565df868fba05
Sheng-Hua Chen <47971203+schen38@users.noreply.github.com>
2020-04-11 18:36:25 +0800

    Update circleChartDB.js

--- js/circleChartDB.js
+++ js/circleChartDB.js
@@ -4,10 +4,9 @@ $(window).on('scroll',function() {
     wH2 = $(window).height(),
     wS2 = $(this).scrollTop();
     if (wS2 > (hT2+hH2-wH2) && (hT2 > wS2) && (wS2+wH2 > hT2+hH2)){
-
       $(".circleChart#8").circleChart({
         size: 50,
-        value: 75,
+        value: 80,
         text: 0,
         onDraw: function(el, circle) {
           circle.text(Math.round(circle.value) + "%");
