c89ebacc66bdee837c06e80a91bf0d178d67a9ca
Sheng-Hua Chen <47971203+schen38@users.noreply.github.com>
2020-04-11 18:37:36 +0800

    Update experiences.js

--- js/experiences.js
+++ js/experiences.js
@@ -1,38 +1,44 @@
+$(window).scroll(function(){
+  var shouldBeVisible0 = $(window).scrollTop()>450;
+  if (shouldBeVisible0 ) {
+      $('.content0').css({visibility:'visible'}).show({fadeIn:'1500'});
+  }else if (!shouldBeVisible0) {
+    $('.content0').hide();
+  }
+});
 
-  $(window).scroll(function(){
-    var hT4 = $('.info2').offset().top,
-    hH4 = $('.info2').outerHeight(),
-    wH = $(window).height(),
-    wS = $(this).scrollTop();
-    if (wS > (hT4+hH4-wH) && (hT4 > wS) && (wS+wH > hT4+hH4)){
-      $('.content1').hide().fadeIn(900, function() {
-        $(this).css('visibility','visible');
-      });
-    }
-    hT5 = $('.git').offset().top,
-    hH5 = $('.git').outerHeight();
+$(window).scroll(function(){
+  var shouldBeVisible1 = $(window).scrollTop()>800;
+  if (shouldBeVisible1 ) {
+      $('.content1').css({visibility:'visible'}).show({fadeIn:'1500'});
+  }else if (!shouldBeVisible1) {
+    $('.content1').hide();
+  }
+});
 
-    if (wS > (hT5+hH5-wH) && (hT5 > wS) && (wS+wH > hT5+hH5)){
-      $('.content2').hide().fadeIn(1500, function() {
-        $(this).css('visibility','visible');
-      });
-    }
+$(window).scroll(function(){
+  var shouldBeVisible2 = $(window).scrollTop()>1150;
+  if (shouldBeVisible2 ) {
+      $('.content2').css({visibility:'visible'}).show({fadeIn:'1500'});
+  }else if (!shouldBeVisible2) {
+    $('.content2').hide();
+  }
+});
 
-    hT6 = $('.collapse').offset().top,
-    hH6 = $('.collapse').outerHeight();
+$(window).scroll(function(){
+  var shouldBeVisible3 = $(window).scrollTop()>1500;
+  if (shouldBeVisible3 ) {
+      $('.content3').css({visibility:'visible'}).show({fadeIn:'1500'});
+  }else if (!shouldBeVisible3) {
+    $('.content3').hide();
+  }
+});
 
-    if (wS > (hT6+hH6-wH) && (hT6 > wS) && (wS+wH > hT6+hH6)){
-      $('.content3').hide().fadeIn(1950, function() {
-        $(this).css('visibility','visible');
-      });
-    };
-
-    hT7 = $('.codeweb').offset().top,
-    hH7 = $('.codeweb').outerHeight();
-
-    if (wS > (hT7+hH7-wH) && (hT7 > wS) && (wS+wH > hT7+hH7)){
-      $('.content4').hide().fadeIn(1550, function() {
-        $(this).css('visibility','visible');
-      });
-    };
-  });
+$(window).scroll(function(){
+  var shouldBeVisible4 = $(window).scrollTop()>1850;
+  if (shouldBeVisible4 ) {
+      $('.content4').css({visibility:'visible'}).show({fadeIn:'1500'});
+  }else if (!shouldBeVisible4) {
+    $('.content4').hide();
+  }
+});
